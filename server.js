e11f1e7ec55f406c9f408e6dc4aea5e8ce1b9a04
makewebme <makewebme@gmail.com>
2020-06-01 10:21:02 +0300

    After lesson 13

--- server.js
+++ server.js
@@ -2,31 +2,42 @@ const express = require('express')
 const os = require('os')
 const app = express()
 const router = express.Router()
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const formData = require('express-form-data')
+const isLogged = require('./middlewares/isLogged')
 
 
+// DB
 
 const mongoose = require('mongoose')
-
 
 const { dbUrl, dbName } = require('./consts')
 mongoose.connect(`${dbUrl}/${dbName}`, { useNewUrlParser: true })
 const db = mongoose.connection
 
 
+// View Engine
 
 app.set('view engine', 'pug')
 
-app.use(bodyParser())
-app.use(formData.parse({
-  uploadDir: os.tmpdir(),
-  autoClean: true
-}))
 
+// Middlware
+
+app.use(bodyParser())
+app.use(formData.parse({ uploadDir: os.tmpdir(), autoClean: true }))
 app.use(express.static('public'))
+app.use(session({ secret: 'our_secret', resave: false, saveUninitialized: true }))
+
+// Check login status middleware
+
+
+// Routes
 
 app.use('/', require('./routes/static'))
-app.use('/api', require('./routes/api'))
+app.use('/api', isLogged, require('./routes/api'))
+
+
+// Listen
 
 app.listen(4000, () => console.log(`Example app listening at http://localhost:4000`))
